f87ce094557967cd29fe75526e53b1208dfbe56e
Will Thompson <will@willthompson.co.uk>
2013-10-05 09:24:34 +0100

    Don't use a public search; don't send replies by default

--- fewerror.py
+++ fewerror.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, Stream, API
+import argparse
 import os
 import sys
 import itertools
@@ -34,8 +35,8 @@ def make_reply(text):
     tags_from_less = itertools.dropwhile((lambda (word, tag): word.lower() != 'less'),
                                          blob.tags)
     try:
-        less = tags_from_less.next()
-        w, w_pos = tags_from_less.next()
+        less = next(tags_from_less)
+        w, w_pos = next(tags_from_less)
     except StopIteration:
         return None
 
@@ -46,48 +47,54 @@ def make_reply(text):
         return None
 
     # Avoid replying "fewer lonely" to "less lonely girl"
-    try:
-        v, v_pos = tags_from_less.next()
-        if v_pos == 'NN':
-            return None
-    except StopIteration:
-        pass
+    v, v_pos = next(tags_from_less, (None, None))
+    if v_pos == 'NN':
+        return None
 
-    return u'I think you mean “fewer %s”.' % (w.lower())
+    return w.lower()
 
 
 class LessListener(StreamListener):
     TIMEOUT = datetime.timedelta(seconds=120)
 
-    def __init__(self, *args):
-        StreamListener.__init__(self, *args)
-
+    def __init__(self, *args, **kwargs):
+        self.post_replies = kwargs.pop('post_replies', False)
+        StreamListener.__init__(self, *args, **kwargs)
         self.last = datetime.datetime.now() - self.TIMEOUT
 
     def on_connect(self):
         print "connected."
 
     def on_status(self, status):
-        print status.text
-
         now = datetime.datetime.now()
-        if now - self.last < self.TIMEOUT:
+        if self.post_replies and now - self.last < self.TIMEOUT:
             return
 
-        bare_reply = make_reply(status.text)
-        if bare_reply is None:
+        quantity = make_reply(status.text)
+        if quantity is None:
             return
 
-        reply = u"@%s %s" % (status.author.screen_name, bare_reply)
+        reply = u'@%s I think you mean “fewer %s”.' % (status.author.screen_name, quantity)
 
         if len(reply) <= 140:
+            print status.text
             print '--> %s' % reply
-            r = self.api.update_status(reply, in_reply_to_status_id=status.id)
-            print "  https://twitter.com/_/status/%s" % r.id
-            self.last = now
+
+            if self.post_replies:
+                r = self.api.update_status(reply, in_reply_to_status_id=status.id)
+                print "  https://twitter.com/_/status/%s" % r.id
+                self.last = now
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=u'annoy some tweeps',
+                                     epilog='Note that --post-replies --use-public-stream will get you banned pretty quickly')
+    parser.add_argument('--post-replies', action='store_true',
+                        help='post (rate-limited) replies, rather than just printing them locally')
+    parser.add_argument('--use-public-stream', action='store_true',
+                        help='search public tweets for "less", rather than your own stream')
+    args = parser.parse_args()
+
     consumer_key = os.environ["CONSUMER_KEY"]
     consumer_secret = os.environ["CONSUMER_SECRET"]
 
@@ -98,8 +105,10 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
 
     api = API(auth)
-    l = LessListener(api)
+    l = LessListener(api, post_replies=args.post_replies)
 
     stream = Stream(auth, l)
-    #stream.filter(track=['less'])
-    stream.userstream()
+    if args.use_public_stream:
+        stream.filter(track=['less'])
+    else:
+        stream.userstream()
