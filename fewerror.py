91dbf5c4a0ffcb6873a779ffb883a379c82ca037
Will Thompson <will@willthompson.co.uk>
2014-03-26 23:13:40 +0000

    Wow POS

--- fewerror.py
+++ fewerror.py
@@ -53,18 +53,125 @@ def make_reply(text):
 
 
 class POS:
-    # adjective or numeral, ordinal
+    """
+    https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+    http://www.surdeanu.info/mihai/teaching/ista555-fall13/readings/PennTreebankTagset.html
+    """
+
+    # 1. Coordinating conjunction
+    CC = 'CC'
+
+    # 2. Cardinal number
+    CD = 'CD'
+
+    # 3. Determiner
+    DT = 'DT'
+
+    # 4. Existential there
+    EX = 'EX'
+
+    # 5. Foreign word
+    FW = 'FW'
+
+    # 6. Preposition or subordinating conjunction
+    IN = 'IN'
+
+    # 7. Adjective or numeral, ordinal
     JJ = 'JJ'
 
-    # verb, past participle
-    VBN = 'VBN'
+    # 8. Adjective, comparative
+    JJR = 'JJR'
 
-    # noun, proper, singular
-    NNP = 'NNP'
+    # 9. Adjective, superlative
+    JJS = 'JJS'
+
+    # 10. List item marker
+    LS = 'LS'
+
+    # 11. Modal
+    MD = 'MD'
 
     # Unfortunately there is no POS tag for mass nouns specifically:
-    # noun, singular or mass
+    # 12. Noun, singular or mass
     NN = 'NN'
+
+    # 13. Noun, plural
+    NNS = 'NNS'
+
+    # 14. Proper noun, singular
+    NNP = 'NNP'
+
+    # 15. Proper noun, plural
+    NNPS = 'NNPS'
+
+    # 16. Predeterminer
+    PDT = 'PDT'
+
+    # 17. Possessive ending
+    POS = 'POS'
+
+    # 18. Personal pronoun
+    PRP = 'PRP'
+
+    # 19. Possessive pronoun
+    PRP_ = 'PRP$'
+
+    # 20. Adverb
+    RB = 'RB'
+
+    # 21. Adverb, comparative
+    RBR = 'RBR'
+
+    # 22. Adverb, superlative
+    RBS = 'RBS'
+
+    # 23. Particle
+    RP = 'RP'
+
+    # 24. Symbol
+    SYM = 'SYM'
+
+    # 25. to
+    TO = 'TO'
+
+    # 26. Interjection
+    UH = 'UH'
+
+    # 27. Verb, base form
+    VB = 'VB'
+
+    # 28. Verb, past tense
+    VBD = 'VBD'
+
+    # 29. Verb, gerund or present participle
+    VBG = 'VBG'
+
+    # 30. Verb, past participle
+    VBN = 'VBN'
+
+    # 31. Verb, non-3rd person singular present
+    VBP = 'VBP'
+
+    # 32. Verb, 3rd person singular present
+    VBZ = 'VBZ'
+
+    # 33. Wh-determiner
+    WDT = 'WDT'
+
+    # 34. Wh-pronoun
+    WP = 'WP'
+
+    # 35. Possessive wh-pronoun
+    WP_ = 'WP$'
+
+    # 36. Wh-adverb
+    WRB = 'WRB'
+
+    @staticmethod
+    def nounish(pos):
+        # nltk apparently defaults to 'NN' for smileys :) so special-case those
+        return pos in (POS.NN, POS.NNS, POS.NNP, POS.NNPS) and \
+            any(c.isalpha() for c in v)
 
 
 def find_an_indiscrete_quantity(blob):
