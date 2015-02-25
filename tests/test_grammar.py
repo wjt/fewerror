35301d8f6e7da0b446f56f2fa383b459e967a170
Will Thompson <will@willthompson.co.uk>
2015-02-25 09:00:15 +0000

    Wire up actual test cases

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,7 +1,6 @@
 # vim: fileencoding=utf-8
 import fewerror
 import codecs
-import sys
 import string
 import pytest
 
@@ -11,10 +10,9 @@ def tweets_from(filename):
     with codecs.open(filename, 'r', 'utf-8') as f:
         for tweet in f:
             tweet = tweet.strip()
-            try:
-                reply = fewerror.make_reply(tweet)
-            except:
-                reply = None
+            reply = None
+            for reply in fewerror.make_reply(tweet):
+                break
             yield (tweet, reply)
 
 def do(filename, expect_replies=None):
@@ -55,16 +53,6 @@ def test_true_positives(tweet, reply):
 def test_false_positives(tweet, reply):
     check(tweet,reply, expect_replies=False)
 
-def test_false_positives():
-    do('false-positive.txt', expect_replies=False)
-
 @pytest.mark.parametrize("tweet,reply", tweets_from('false-negative.txt'))
 def test_false_negatives(tweet, reply):
     check(tweet,reply, expect_replies=True)
-
-if __name__ == '__main__':
-    for name in sys.argv[1:]:
-        print name
-        print "=" * len(name)
-        do(name, expect_replies=None)
-        print
--- tests/test_state.py
+++ tests/test_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import fewerror
-import os
 
 def test_stateholder(tmpdir):
     with tmpdir.as_cwd():
