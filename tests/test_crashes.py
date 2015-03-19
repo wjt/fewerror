bfc5b6dda4dc29123ee5d78195748d511e259653
Will Thompson <will@willthompson.co.uk>
2015-03-19 10:38:12 +0000

    test_crashes: better test name

--- tests/test_crashes.py
+++ tests/test_crashes.py
@@ -3,14 +3,12 @@
 import pytest
 import fewerror
 
-crashy_tweets = [
-'''2x more likely to hire men than women, even when he's less capable http://t.co/XcovCXpsqC
+crashy_tweet = '''2x more likely to hire men than women, even when he's less capable http://t.co/XcovCXpsqC
 
-"We shouldn't sacrifice quality to hire women!"''',
-]
+"We shouldn't sacrifice quality to hire women!"'''
 
-@pytest.mark.parametrize("tweet", crashy_tweets)
-def test_not_crashing(tweet):
-    for reply in fewerror.make_reply(tweet):
+
+def test_not_crashing():
+    for reply in fewerror.make_reply(crashy_tweet):
         # if we get anything out at all, it should be 'fewer capable'
         assert reply == 'fewer capable'
