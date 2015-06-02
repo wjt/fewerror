6c1c8e354f8b6db76717a0ec008b92738aa03298
Will Thompson <will@willthompson.co.uk>
2015-06-02 08:09:29 +0100

    get_oauth_token: make it work

--- get_oauth_token.py
+++ get_oauth_token.py
@@ -4,18 +4,17 @@ from six.moves import input
 
 consumer_key = os.environ["CONSUMER_KEY"]
 consumer_secret = os.environ["CONSUMER_SECRET"]
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret, secure=True)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
 redirect_url = auth.get_authorization_url()
 print("go to %s" % redirect_url)
 
-verifier = eval(input('Verifier:'))
+verifier = input('Verifier:')
 
 try:
-    auth.get_access_token(verifier)
+    access_token, access_token_secret = auth.get_access_token(verifier)
+    print(u'ACCESS_TOKEN="%s"' % access_token)
+    print(u'ACCESS_TOKEN_SECRET="%s"' % access_token_secret)
 except tweepy.TweepError as e:
     print('Error! Failed to get access token.')
     print(e)
-
-print('ACCESS_TOKEN="%s"' % auth.access_token.key)
-print('ACCESS_TOKEN_SECRET="%s"' % auth.access_token.secret)
