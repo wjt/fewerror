119243e6c95aa0bc9c97ca9fa4628cd00f34b6b1
Will Thompson <will@willthompson.co.uk>
2016-11-18 11:30:06 +0000

    update.sh: refresh corpora

--- update.sh
+++ update.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+# vim: sts=4 sw=4 et
 set -e
 set -x
 cd $(dirname "$0")
-git remote update
-git checkout -qf $1
+
+if [[ "$#" -gt 1 ]]; then
+    git remote update
+    git checkout -qf $1
+fi
+
 ../env/bin/pip install -r requirements.txt
+../env/bin/python -m textblob.download_corpora
 sudo systemctl restart fewerror-twitter fewerror-telegram
