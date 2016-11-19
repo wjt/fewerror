b8f572f5008a3250d6bab6f97bce0a1873a4b230
Will Thompson <will@willthompson.co.uk>
2016-11-19 08:53:11 +0000

    update.sh: run updated version after git checkout

--- update.sh
+++ update.sh
@@ -7,10 +7,11 @@ cd $(dirname "$0")
 if [[ "$#" -ge 1 ]]; then
     git remote update
     git checkout -qf $1
+    exec "$0"
+else
+    ../env/bin/pip install --upgrade pip wheel
+    ../env/bin/pip install -r requirements.txt
+    ../env/bin/python -m textblob.download_corpora
+
+    sudo systemctl restart fewerror-twitter fewerror-telegram
 fi
-
-../env/bin/pip install --upgrade pip wheel
-../env/bin/pip install -r requirements.txt
-../env/bin/python -m textblob.download_corpora
-
-sudo systemctl restart fewerror-twitter fewerror-telegram
