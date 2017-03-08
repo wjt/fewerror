12e53947081b7c62099c2307c3febf9856ce23b6
Will Thompson <will@willthompson.co.uk>
2017-03-08 09:28:43 +0000

    @gnuerror: extremely noddy validation

--- cbdq/gnewline.py
+++ cbdq/gnewline.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import argparse
+import re
 import sys
 import json
 import yaml
 
 
 skip_keys = frozenset(('QUOTE', 'SPACE', '00-SOURCE'))
+traceryish_rx = re.compile(r'#(\w+)(?:\.\w+)*#')
 
 
 def fmap(f, val):
@@ -17,6 +19,21 @@ def fmap(f, val):
 
 def space(val):
     return val.replace(' ', '#SPACE#')
+
+
+def validate(j):
+    used = {'origin'} | skip_keys
+    for k, v in j.items():
+        def _validate(val):
+            for var in traceryish_rx.findall(val):
+                if var not in j:
+                    raise ValueError(k, val, var)
+                used.add(var)
+        fmap(_validate, v)
+
+    unused = j.keys() - used
+    if unused:
+        raise ValueError(unused)
 
 
 def transform(j):
@@ -32,6 +49,7 @@ def main():
     a = p.parse_args()
 
     j = yaml.load(a.source)
+    validate(j)
     j_ = transform(j)
     json.dump(j_, sys.stdout, indent=2, sort_keys=True)
 
