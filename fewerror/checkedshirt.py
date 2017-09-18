a5fcc2e90b380e92547dfd90af00499d64b0eb97
Will Thompson <will@willthompson.co.uk>
2017-09-18 11:03:15 +0100

    checkedshirt: add --log-level

--- fewerror/checkedshirt.py
+++ fewerror/checkedshirt.py
@@ -4,11 +4,14 @@ import logging
 
 
 def add_arguments(parser):
-    g = parser.add_argument_group('logging')
+    g = parser.add_argument_group('logging').add_mutually_exclusive_group()
     g.add_argument('--log-config',
                    type=argparse.FileType('r'),
                    metavar='FILE.json',
-                   help='Read logging config from FILE.json (default: DEBUG to stdout)')
+                   help='Read logging config from FILE.json (default: LEVEL to stderr)')
+    g.add_argument('--log-level',
+                   default='DEBUG',
+                   help='Log at this level to stderr (default: DEBUG)')
 
 
 def init(args):
@@ -16,5 +19,5 @@ def init(args):
         log_config = json.load(args.log_config)
         logging.config.dictConfig(log_config)
     else:
-        logging.basicConfig(level='DEBUG',
+        logging.basicConfig(level=args.log_level,
                             format='%(asctime)s %(levelname)8s [%(name)s] %(message)s')
