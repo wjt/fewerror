2bf0f99246f9f8341a1a196fd3c10359f5c7b866
Will Thompson <will@willthompson.co.uk>
2018-02-09 23:43:23 +0000

    Update update

--- update.sh
+++ update.sh
@@ -10,18 +10,7 @@ if [[ "$#" -ge 1 ]]; then
     git checkout -qf $1
     exec flock $PARENT/update.lock "$0"
 else
-    ENV_LINK=$PARENT/env
-    ENV_DIR=$(mktemp -d -p "$PARENT" env.XXXXXXXXXX)
-    PYTHON3="$(which python3)"
-
-    virtualenv --python="$PYTHON3" "$ENV_DIR"
-
-    $ENV_DIR/bin/pip install --upgrade pip wheel
-    $ENV_DIR/bin/pip install -r requirements.txt
-    $ENV_DIR/bin/python -m textblob.download_corpora
-
-    OLD_ENV_DIR="$(readlink -f "$ENV_LINK")"
-    ln -s -T --force "$ENV_DIR" "$ENV_LINK"
-    rm -rf "$OLD_ENV_DIR"
+    pipenv install
+    pipenv run python -m textblob.download_corpora
     #sudo systemctl restart fewerror-twitter
 fi
